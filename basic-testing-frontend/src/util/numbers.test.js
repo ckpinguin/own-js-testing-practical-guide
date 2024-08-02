2a0f52453fb0efce29c12a0d4a896e96a642187f
Christof Kälin <christof.kaelin@protonmail.com>
2024-08-02 20:36:43 +0200

    some more practice with simple functions

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -7,12 +7,19 @@ it("should make a number out of a string number", () => {
   const result = transformToNumber(arg)
 
   expect(result).toBeTypeOf("number")
+  // Test this too, because NaN as also of type number
+  expect(result).toBe(1)
+  // more generic:
+  expect(result).toBe(+arg)
 })
 
 it("should yield NaN for a non-transformable value", () => {
-  const arg = "one!"
+  const input = "one!"
+  const input2 = {}
 
-  const result = transformToNumber(arg)
+  const result = transformToNumber(input)
+  const result2 = transformToNumber(input2)
 
   expect(result).toBeNaN()
+  expect(result2).toBeNaN()
 })
