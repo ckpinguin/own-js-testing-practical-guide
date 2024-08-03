41aeef8a90072a3e677be9027b04623fd2cbd9d4
Christof Kälin <christof.kaelin@protonmail.com>
2024-08-03 14:39:25 +0200

    refactor: Improve validation functions and tests
    Refactor the validation functions in the `validation.js` file to improve code readability and maintainability. Also, update the corresponding tests in the `validation.test.js` file to ensure proper validation behavior.

--- basic-testing-frontend/src/util/validation.test.js
+++ basic-testing-frontend/src/util/validation.test.js
@@ -1,35 +1,37 @@
-import { it, expect } from "vitest"
+import { it, expect, describe } from "vitest"
 
 import { validateNumber, validateStringNotEmpty } from "./validation"
 
-// validateStringNotEmpty
-it("should throw an error with a message that contains ar reason (must not be empty)", () => {
-  const input = ""
-  const validationFn = () => validateStringNotEmpty(input)
-  expect(validationFn).toThrow(/must not be empty/)
+describe("validateStringNotEmpty()", () => {
+  it("should throw an error with a message that contains ar reason (must not be empty)", () => {
+    const input = ""
+    const validationFn = () => validateStringNotEmpty(input)
+    expect(validationFn).toThrow(/must not be empty/)
+  })
+
+  it("should throw an error if a long string of blanks is provided", () => {
+    const input = ""
+    const validationFn = () => validateStringNotEmpty(input)
+    expect(validationFn).toThrow()
+  })
 })
 
-it("should throw an error if a long string of blanks is provided", () => {
-  const input = ""
-  const validationFn = () => validateStringNotEmpty(input)
-  expect(validationFn).toThrow()
-})
+describe("validateNumber()", () => {
+  it("should throw an error if a non-numeric value is provided", () => {
+    const input = "one!"
+    const validationFn = () => validateNumber(input)
+    expect(validationFn).toThrow()
+  })
 
-// validateNumber
-it("should throw an error if a non-numeric value is provided", () => {
-  const input = "one!"
-  const validationFn = () => validateNumber(input)
-  expect(validationFn).toThrow()
-})
+  it("should throw an error if NaN is provided", () => {
+    const input = NaN
+    const validationFn = () => validateNumber(input)
+    expect(validationFn).toThrow(/invalid number input./i)
+  })
 
-it("should throw an error if NaN is provided", () => {
-  const input = NaN
-  const validationFn = () => validateNumber(input)
-  expect(validationFn).toThrow(/invalid number input./i)
-})
-
-it("should not throw an error when provided with a number", () => {
-  const input = 4
-  const validationFn = () => validateNumber(input)
-  expect(validationFn).not.toThrow()
+  it("should not throw an error when provided with a number", () => {
+    const input = 4
+    const validationFn = () => validateNumber(input)
+    expect(validationFn).not.toThrow()
+  })
 })
