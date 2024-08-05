bdaea6cbc066257ec6dec604267a9c25c8baf4ac
Christof Kälin <christof.kaelin@protonmail.com>
2024-08-05 14:38:11 +0200

    async test with try/await

---
+++ deepdive_mocking/util/http.test.js
@@ -0,0 +1,40 @@
+import { it, expect, vi } from "vitest"
+import { sendDataRequest } from "./http"
+
+const testResponseData = { testkey: "testData" }
+const testFetch = vi.fn((url, options) => {
+  return new Promise((resolve, reject) => {
+    if (typeof options.body !== "string") {
+      return reject("Not a string.")
+    }
+    const testResponse = {
+      ok: true,
+      json() {
+        return new Promise((resolve, reject) => {
+          resolve(testResponseData)
+        })
+      },
+    }
+    resolve(testResponse)
+  })
+})
+
+vi.stubGlobal("fetch", testFetch)
+
+it("should return any available response data", () => {
+  const testData = { key: "test" }
+
+  return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData)
+})
+
+it("should convert the provided data to JSON before sending the request", async () => {
+  const testData = { key: "test" }
+  let errorMessage
+  try {
+    await sendDataRequest(testData)
+  } catch (error) {
+    errorMessage = error
+  }
+
+  expect(errorMessage).not.toBe("Not a string.")
+})
