eb73e7e33bfe29c45ba9ddea5ad389d8a5a44814
Christof Kälin <christof.kaelin@protonmail.com>
2024-08-05 12:40:57 +0200

    mocking implementation

--- sideeffects-testing/src/util/io.test.js
+++ sideeffects-testing/src/util/io.test.js
@@ -3,6 +3,15 @@ import writeData from "./io"
 import { promises as fs } from "fs"
 
 vi.mock("fs") // automock
+vi.mock("path", () => {
+  return {
+    default: {
+      join: (...args) => {
+        return args[args.length - 1]
+      },
+    },
+  }
+})
 
 it("should execute the writeFile method", () => {
   const testData = "test"
@@ -11,4 +20,12 @@ it("should execute the writeFile method", () => {
   writeData(testData, testFilename)
 
   expect(fs.writeFile).toBeCalled()
+})
+
+it("should execute the writeFile method with the right arguments", () => {
+  const testData = "test"
+  const testFilename = "test.txt"
+
+  writeData(testData, testFilename)
+  expect(fs.writeFile).toBeCalledWith(testFilename, testData)
 })
