1c77311b5513b9ed4596eea90740e9e6a7523674
Christof Kälin <christof.kaelin@protonmail.com>
2024-08-05 14:00:58 +0200

    more tests

--- deepdive_mocking/util/validation.test.js
+++ deepdive_mocking/util/validation.test.js
@@ -1,18 +1,35 @@
-import { it, expect } from "vitest"
+import { it, expect, test } from "vitest"
 import { validateNotEmpty } from "./validation"
 
 it("should throw an error when an empty string is provided", () => {
   const inputStr = ""
-  const resultFn = () => {
+  const validationFn = () => {
     validateNotEmpty(inputStr)
   }
-  expect(resultFn).toThrow()
+  expect(validationFn).toThrow()
+})
+
+it("should throw an error when a string with only blanks is provided", () => {
+  const inputStr = "    "
+  const validationFn = () => {
+    validateNotEmpty(inputStr)
+  }
+  expect(validationFn).toThrow()
+})
+
+it("should throw an error with the provided message when an error is thrown", () => {
+  const inputStr = ""
+  const testErrorMessage = "Test"
+  const validationFn = () => {
+    validateNotEmpty(inputStr, testErrorMessage)
+  }
+  expect(validationFn).toThrow(testErrorMessage)
 })
 
 it("should not throw when a non-empty string is provided", () => {
   const inputStr = "Test"
-  const resultFn = () => {
+  const validationFn = () => {
     validateNotEmpty(inputStr)
   }
-  expect(resultFn).not.toThrow()
+  expect(validationFn).not.toThrow()
 })
